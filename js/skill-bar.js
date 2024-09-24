1b39ae5f888cd684cc600faa7a83050b737d13de
Julio <aprendendogit@edu.com>
2024-09-24 15:17:27 -0300

    Skills Section Update

--- js/skill-bar.js
+++ js/skill-bar.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const progressBars = document.querySelectorAll('.progress-bar');
-    
-    progressBars.forEach(bar => {
-        const skillValue = bar.getAttribute('data-skill');
-        bar.style.width = skillValue; // Define a largura da barra
-    });
+
+    // Função para ativar a animação
+    function animateProgressBars() {
+        progressBars.forEach(bar => {
+            const skillValue = bar.getAttribute('data-skill') + '%'; // Obtém o valor do atributo data-skill
+            bar.style.width = skillValue; // Aplica a largura da barra
+        });
+    }
+
+    // Adiciona um pequeno atraso para garantir que a animação seja visível
+    setTimeout(animateProgressBars, 100); // 100ms de atraso para garantir a animação suave
 });
